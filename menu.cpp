d5e4c635e3ee1fe07782696e1dbc460432e214f4
MrAziz2009 <Cool_zizo2009@hotmail.com>
2018-11-06 19:52:31 -0500

    Add files via upload

--- menu.cpp
+++ menu.cpp
@@ -2,59 +2,38 @@
 using namespace std; 
 
 /*
-Author:
+Author Aziz Aldawk 
 This is for our project and my mission is doing menu function.
-should only funciton on main to make it easy.....
-note: it may change something on commend.....  
+shll only funciton on main to make it easy.....
+Note: maybe i need to gethering all program on once. 
+Note: This program need to be done on the weekend. 
 */
 void Menu(); 
 int main(){
 
+//shll only function here. 
 
-Menu();
-//call function for menu...
+Menu(); 
+// Great, my function is working and Shll i start so. :) 
+// make sure is looping until select (0); 
+
 }
-
-/*================= Menu Function ===============
-
- This function supposed to be chose what the user need 
- and  which the function.  
- For example, if user want adding, he/she 
- must chose number 1 to apply adding functing
- and can use it. 
-if user want to exit, he/she may select zero
-to stop the loop and exit. 
-==============================================*/
-
+ 
 void Menu(){
 
 int number; 
-do{
-cout<<"If you want  <1>"<<endl
-    <<"if you want  <2>"<<endl
-    <<"if you want  <3>"<<endl
-    <<" Exit? Enter number 0 "<<endl; 
-// please ignore output, i will fix it when i get the functions from you.
-cin>>number; 
+
+
+
 switch(number)
 {
 case 1:
 
-// function call right here
 break;
 
-case 2:
-// function call right here
-break;
 
-case 3: 
-// function call  right here
-break;
-} // This is testing area only before applied the final one....
-//Also, function works very well and just need to add call functions from other group member.
-.
-// please note: i may add more case if there is more call functions..... :)
+}
 
-}while(number != 0); // assume user select zero, loop will stop and exit. 
+
 
 }
